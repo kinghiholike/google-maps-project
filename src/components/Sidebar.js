d3809ae5f8a45f0588982a60760ddacd7bb16de9
kinghiholike <etangoking6@gmail.com>
2023-08-17 12:23:56 +0100

    Sidebar css to make it more interactive and responsive.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Sidebar.css'; // Import your custom CSS file for styling
 
 function Sidebar(props) {
+  const [selectedCategory, setSelectedCategory] = useState('restaurants');
+
   const handleCategoryChange = (event) => {
-    props.onCategoryChange(event.target.value);
+    const selectedValue = event.target.value;
+    setSelectedCategory(selectedValue);
+    props.onCategoryChange(selectedValue);
   };
 
+  const categoryOptions = [
+    { value: 'restaurants', label: 'Restaurants' },
+    { value: 'parks', label: 'Parks' },
+    { value: 'hotels', label: 'Hotels' },
+    // Add more category options
+  ];
+
   return (
-    <div>
-      <select onChange={handleCategoryChange}>
-        <option value="restaurants">Restaurants</option>
-        <option value="parks">Parks</option>
-        <option value="hotels">Hotels</option>
-        {/* Add other category options */}
+    <div className="sidebar">
+      <h2>Explore Categories</h2>
+      <select className="category-dropdown" value={selectedCategory} onChange={handleCategoryChange}>
+        {categoryOptions.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
       </select>
+      <p className="selected-feedback">You've selected: {selectedCategory}</p>
     </div>
   );
 }
